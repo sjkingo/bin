273fdc3fc1dd9d18db0dfb3bd58d6f6dd41694d7
Sam Kingston <sam@sjkwi.com.au>
2014-11-19 13:41:34 +1000

    delete unused bin files

--- sync_fedora.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DEST="/srv/fedora-mirror/mirror"
-RSYNC_MIRROR="rsync://mirror.aarnet.edu.au/fedora/linux"
-EXCLUDE="$DEST/../EXCLUDE"
-
-rsync -av -H --exclude-from=${EXCLUDE} --numeric-ids --delete --delete-after --delay-updates \
-    ${RSYNC_MIRROR} ${DEST}
