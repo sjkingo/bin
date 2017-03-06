de71541faaa5cab6f680b40f38b6577badf74bf9
Sam Kingston <sam@sjkwi.com.au>
2017-03-06 15:34:13 +1000

    switch to feh for background setting

--- rotate_wallpaper.sh
+++ rotate_wallpaper.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[[ ! -z "$DISPLAY" ]] && DISPLAY=:0 /home/sam/bin/awsetbg -a -r /home/sam/Dropbox/Photos/Wallpapers/landscape/
+[[ ! -z "$DISPLAY" ]] && DISPLAY=:0 feh --randomize --bg-fill --no-fehbg ~/Dropbox/Photos/Wallpapers/landscape
